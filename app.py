84e1b841f8ec0057f2b53dd46821e918f3dedc56
Ansh Kushwaha <ansh.kushwaha26@gmail.com>
2025-04-03 21:52:55 +0530

    fix: deployment fix #2

--- app.py
+++ app.py
@@ -299,4 +299,5 @@ def download(session_id):
     )
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port, debug=False, allow_unsafe_werkzeug=True)
